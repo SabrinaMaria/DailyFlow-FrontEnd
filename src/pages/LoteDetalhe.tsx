e26c54ce411e9eaf33c3a11b40057c2bc6d8838d
SabrinaMaria <sabrinasantosdasilva662@gmail.com>
2021-03-18 19:39:33 -0300

    Atualizações

--- src/pages/LoteDetalhe.tsx
+++ src/pages/LoteDetalhe.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, TextInput, Alert, Button, Pressable } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Alert, Pressable } from 'react-native';
 
 export default function LoteDetalhe() {
     return (
@@ -18,19 +18,16 @@ export default function LoteDetalhe() {
                     <TextInput
                         style={styles.input}
                         placeholder="DD/MM/AAAA"
-                        keyboardType="numeric"
                     />
                     <Text>Previsão de entrega</Text>
                     <TextInput
                         style={styles.input}
                         placeholder="DD/MM/AAAA"
-                        keyboardType="numeric"
                     />
                     <Text>Data de entrega</Text>
                     <TextInput
                         style={styles.input}
                         placeholder="DD/MM/AAAA"
-                        keyboardType="numeric"
                     />
                     <Text>Tamanho previsto</Text>
                     <TextInput style={styles.input}
@@ -45,37 +42,31 @@ export default function LoteDetalhe() {
                     <Text>Peso de entrada</Text>
                     <TextInput style={styles.input}
                         placeholder="Digite aqui"
-                        keyboardType="numeric"
                     />
                     <Text>Ração inicial</Text>
                     <TextInput
                         style={styles.input}
                         placeholder="DD/MM/AAAA"
-                        keyboardType="numeric"
                     />
                     <Text>Ração C1</Text>
                     <TextInput
                         style={styles.input}
                         placeholder="DD/MM/AAAA"
-                        keyboardType="numeric"
                     />
                     <Text>Ração C2</Text>
                     <TextInput
                         style={styles.input}
                         placeholder="DD/MM/AAAA"
-                        keyboardType="numeric"
                     />
                     <Text>Ração final</Text>
                     <TextInput
                         style={styles.input}
                         placeholder="DD/MM/AAAA"
-                        keyboardType="numeric"
                     />
                     <Text>Início do horário de jejum</Text>
                     <TextInput
                         style={styles.lastInput}
                         placeholder="DD/MM/AAAA"
-                        keyboardType="numeric"
                     />
                 </View>
 
@@ -83,6 +74,21 @@ export default function LoteDetalhe() {
                     <Pressable style={styles.pressable} onPress={() => Alert.alert('Nada')}>
                         <Text style={styles.textButton2}>ADICIONAR ATUALIZAÇÃO</Text>
                     </Pressable>
+                </View>
+
+                <View style={styles.middleView2}>
+                    <View style={styles.atualizacaoColumn}>
+                        <Text style={styles.textAtualizacao}>Data:</Text>
+                        <Text style={styles.textAtualizacao}>Peso:</Text>
+                        <Text style={styles.textAtualizacao}>Tamanho efetivo:</Text>
+                        <Text style={styles.textAtualizacao}>Mortalidade:</Text>
+                    </View>
+                    <View>
+                        <Text style={styles.textAtualizacao}>18/03/2021</Text>
+                        <Text style={styles.textAtualizacao}>20.560 (kg)</Text>
+                        <Text style={styles.textAtualizacao}>100</Text>
+                        <Text style={styles.textAtualizacao}>12</Text>
+                    </View>
                 </View>
 
                 <StatusBar style="auto" />
@@ -128,7 +134,7 @@ const styles = StyleSheet.create({
     lastInput: {
         height: 40,
         width: 340,
-        marginBottom: 70,
+        marginBottom: 160,
         marginTop: 5,
         borderWidth: 0.3,
         borderRadius: 5,
@@ -137,12 +143,24 @@ const styles = StyleSheet.create({
     middleView: {
         width: 370,
         height: 50,
-        marginBottom: 60,
+        marginBottom: 150,
         backgroundColor: '#F0F8FF',
         justifyContent: 'center',
         alignItems: 'center',
         position: 'absolute',
         bottom: 0
+    },
+    middleView2: {
+        width: 340,
+        height: 80,
+        paddingLeft: 20,
+        marginTop: 100,
+        marginBottom: 60,
+        marginLeft: 10,
+        backgroundColor: 'white',
+        position: 'absolute',
+        bottom: 0,flex: 1,
+        flexDirection: 'row',
     },
     bottomView: {
         width: 370,
@@ -170,6 +188,12 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: '#6495ED',
         fontWeight: 'bold',
+    },
+    atualizacaoColumn: {
+        marginRight: 60,
+    },
+    textAtualizacao: {
+        color: 'grey',
     },
     pressable: {
         marginLeft: -10,
